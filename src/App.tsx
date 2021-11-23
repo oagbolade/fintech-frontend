61f53ef4d996ba19b5c654c397943a40d2c9455e
OladayoAgbolade <dayoagbolade@gmail.com>
2021-11-23 14:59:28 +0100

    Added react charts

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,80 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
+import avatar from "./images/avatar.png";
+// @ts-ignore
+import Chart from "react-apexcharts";
 
 function App() {
+  const [chartData, setChartData] = useState({
+    series: [
+      {
+        name: "Desktops",
+        data: [10, 41, 35, 51, 49, 62, 69, 91, 148],
+      },
+    ],
+    options: {
+      chart: {
+        height: 350,
+        type: "line",
+        zoom: {
+          enabled: false,
+        },
+      },
+      dataLabels: {
+        enabled: false,
+      },
+      stroke: {
+        curve: "straight",
+      },
+      title: {
+        text: "Product Trends by Month",
+        align: "left",
+      },
+      grid: {
+        row: {
+          colors: ["#f3f3f3", "transparent"], // takes an array which will be repeated on columns
+          opacity: 0.5,
+        },
+      },
+      xaxis: {
+        categories: [
+          "Jan",
+          "Feb",
+          "Mar",
+          "Apr",
+          "May",
+          "Jun",
+          "Jul",
+          "Aug",
+          "Sep",
+        ],
+      },
+    },
+  });
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <nav className="text-white container d-flex justify-content-between px-5">
+        <div className="mt-4 d-flex">
+          <i className="mt-2 ml-3 fa fa-globe-asia"></i>
+          <h3>FinTech</h3>
+        </div>
+        <div className="mt-4">
+          <i className="fa fa-bell mr-4"></i>
+          <img className="pl-4 avatar" src={avatar} alt="loading" />
+        </div>
+      </nav>
+      <div>
+        <div id="chart">
+          <Chart
+            options={chartData.options}
+            series={chartData.series}
+            // type="line"
+            height={350}
+          />
+        </div>
+      </div>
+      <div></div>
     </div>
   );
 }
